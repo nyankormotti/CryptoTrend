08e6a5086c372d72c622f142d379e3033621ffa0
Y O <responsibility1129@gmail.com>
2019-08-16 02:29:23 +0900

    デカイスマホサイズのレスポンシブ対応

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -48,7 +48,7 @@
                     @endif
                 </div>
                 <div>
-                    <label class="textfield__label" for="Password">パスワード  ※8桁以上 半角英数字</label>
+                    <label class="textfield__label" for="Password">パスワード</label><span>&nbsp;&nbsp;&nbsp;※半角英数字8桁以上</span>
                 </div>
                 @if($errors->has('password'))
                 <div class="u-err__msg">{{$errors->first('password')}}</div>
